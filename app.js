92e9b1e03f184d5a654f6f4053b86bea0bd61584
Amitpandit8148 <amitpandit0391@gmail.com>
2024-09-09 23:23:19 +0530

    add click event

--- app.js
+++ app.js
@@ -9,13 +9,16 @@ let level = 0;
 
 let h2 = document.querySelector("h2");
 
-document.addEventListener("keypress", function(){
-    if (started == false) {
+document.addEventListener("keypress", startGame);
+document.addEventListener("click", startGame);
+
+function startGame(){
+    if (started === false) {
         console.log("game is started");
         started = true;
     }
     levelUp();
-});
+};
 
 function gameFlash(btn) {
     btn.classList.add("flash");
@@ -36,7 +39,7 @@ function levelUp() {
     level++;
     h2.innerText = `Level ${level}`;
 
-    let randIdx = Math.floor(Math.random() * 3);
+    let randIdx = Math.floor(Math.random() * 4);
     let randColor = btns[randIdx];
     let randBtn = document.querySelector(`.${randColor}`);
     // console.log(randIdx);
